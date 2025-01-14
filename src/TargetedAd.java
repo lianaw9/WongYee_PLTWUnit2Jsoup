e7d2cd2b924a91db21af3041f55efbac52e582bd
Liana <wong3327@mydusd.org>
2025-01-14 00:37:44 -0800

    add weighted target words and customer identification

--- src/TargetedAd.java
+++ src/TargetedAd.java
@@ -2,6 +2,9 @@
  * Problem 2 Sell My Pet Food
  */
 public class TargetedAd {
+  static final int BLANKET = 1;
+  static final int JACKET = 2;
+  static final int COUPONS = 3;
 
   public static void main(String[] args)
   {
@@ -12,13 +15,20 @@ public class TargetedAd {
 
     //Preparing targeted advertisements
     DataCollector data = new DataCollector();
-    String targetProduct = "blanket"; //CHANGE THIS
-    data.setData("socialMediaPosts.txt", targetProduct + "TargetWords.txt");
+    data.setData("socialMediaPosts.txt", "targetWords.txt");
 
     System.out.println("Searching for customers...");
     
-    String allUsernames = "";
-    String dataLine = data.getNextPost(); //csv file heading
+    //main storage for usernames
+    String allUsernames = ""; 
+
+    //specialized storage for usernames
+    String allUsernames_blanket = "";
+    String allUsernames_jacket = "";
+    String allUsernames_coupons = "";
+
+
+    String dataLine = data.getNextPost(); //pass csv file heading
     dataLine = data.getNextPost();
     while (!dataLine.equals("NONE")) {
       //System.out.println(dataLine);
@@ -35,19 +45,51 @@ public class TargetedAd {
       
       
       //check if review includes a target word
-      String targetWord = data.getNextTargetWord();
+      String targetWordandNum = data.getNextTargetWord();
+      String targetWord; //get word only
+      int targetNum; //get num only
+
+      //Catch if targetWordandNum = "NONE" or other input
+      try {
+        targetWord = targetWordandNum.substring(0, targetWordandNum.indexOf(" "));
+        targetNum = Integer.valueOf(targetWordandNum.substring(targetWordandNum.length()-1));
+      } catch (Exception e) {
+        targetWord = "NONE";
+        targetNum = 0;
+      }
+
       while (!targetWord.equals("NONE")) {
         if (review.indexOf(targetWord) != -1) {
           //if the target word is included in the review, add name to list
+          String product = "";
 
           //make sure username is not already included in allUsernames
           if (allUsernames.indexOf(username + ",") == -1) {
             allUsernames += username + ", ";
-            System.out.println("CUSTOMER FOUND: " + username);
+
+            //prepare lists for specialized advertisements
+            if (targetNum == BLANKET) {
+              allUsernames_blanket += username + ", ";
+              product = "blanket";
+            } else if (targetNum == JACKET) {
+              allUsernames_jacket += username + ", ";
+              product = "jacket";
+            } else if (targetNum == COUPONS) {
+              allUsernames_coupons += username + ", ";
+              product = "coupons";
+            }
+            
+            System.out.println("CUSTOMER FOUND: " + username + " - " + product);
           }
           break;
         }
-        targetWord = data.getNextTargetWord();
+        targetWordandNum = data.getNextTargetWord();
+        try {
+          targetWord = targetWordandNum.substring(0, targetWordandNum.indexOf(" "));
+          targetNum = Integer.valueOf(targetWordandNum.substring(targetWordandNum.length()-1));
+        } catch (Exception e) {
+          break; //if there are no more words
+        }
       }
 
       //get next post
@@ -56,11 +98,18 @@ public class TargetedAd {
     //System.out.println(allUsernames);
     
 
-    //PREPARE ADVERTISEMENT METHOD
+    //PREPARE ADVERTISEMENT METHODS
+    ExtractPage blanketAdvertisement = new ExtractPage("blanketAd.txt");
+    String blanketAdString = blanketAdvertisement.getFileContent();
+    data.prepareAdvertisement("blanketTargetMarket.txt", allUsernames_blanket, blanketAdString);
 
-    ExtractPage advertisement = new ExtractPage(targetProduct + "Ad.txt");
-    String advertisementStr = advertisement.getFileContent();
-    data.prepareAdvertisement(targetProduct + "TargetMarket.txt", allUsernames, advertisementStr);
+    ExtractPage jacketAdvertisement = new ExtractPage("jacketAd.txt");
+    String jacketAdString = jacketAdvertisement.getFileContent();
+    data.prepareAdvertisement("jacketTargetMarket.txt", allUsernames_jacket, jacketAdString);
+
+    ExtractPage couponsAdvertisement = new ExtractPage("couponsAd.txt");
+    String couponsAdString = couponsAdvertisement.getFileContent();
+    data.prepareAdvertisement("couponsTargetMarket.txt", allUsernames_coupons, couponsAdString);
   }
 
 }
